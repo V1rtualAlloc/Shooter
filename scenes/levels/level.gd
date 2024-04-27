1f1671670a69d0f6e216d1a2de8765d209419f47
V1rtualAlloc <kocovic.vladimir@gmail.com>
2024-04-28 01:54:33 +0200

    inside level new things

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -21,25 +21,9 @@ func _on_player_laser(pos, direction):
 	laser.rotation_degrees = rad_to_deg(direction.angle()) + 90
 	laser.direction = direction
 	$Projectiles.add_child(laser)
-	$UI.update_laser_text()
-
 
 func _on_player_grenade(pos, direction):
 	var grenade = grenade_scene.instantiate() as RigidBody2D
 	grenade.position = pos
 	grenade.linear_velocity = direction * grenade.speed
 	$Projectiles.add_child(grenade)
-	$UI.update_grenade_text()
-
-
-func _on_house_player_entered():
-	var tween = get_tree().create_tween()
-	tween.set_parallel(true)
-	#tween.tween_property($Player, "modulate:a", 0, 2).from(0.5)
-	tween.tween_property($Player/Camera2D, "zoom",  Vector2(1, 1), 1).set_trans(Tween.TRANS_QUAD)
-
-
-func _on_house_player_exited():
-	var tween = get_tree().create_tween()
-	tween.tween_property($Player/Camera2D, "zoom", Vector2(0.6, 0.6), 2)
-

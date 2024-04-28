156c65b2c571c0efe5b58372f7a0d47ee7b7184a
V1rtualAlloc <kocovic.vladimir@gmail.com>
2024-04-28 03:00:02 +0200

    timer for damage on scout

--- scenes/enemies/scout.gd
+++ scenes/enemies/scout.gd
@@ -4,10 +4,18 @@ var player_nearby: bool = false
 var can_laser: bool = true
 var right_gun_use: bool = true
 
+var health: int = 30
+var vulnerable: bool = true
+
 signal laser(pos, direction)
 
 func hit():
-	print('scout was hit')
+	if vulnerable:
+		health -= 10
+		vulnerable = false
+		$Timers/HitTimer.start()
+	if health <= 0:
+		queue_free()
 
 func _process(_delta):
 	if player_nearby:
@@ -19,7 +27,7 @@ func _process(_delta):
 			var direction: Vector2 = (Globals.player_pos - position).normalized()
 			laser.emit(pos, direction)
 			can_laser = false
-			$LaserCooldown.start()
+			$Timers/LaserTimer.start()
 
 
 func _on_attack_area_body_entered(_body):
@@ -30,5 +38,8 @@ func _on_attack_area_body_exited(_body):
 	player_nearby = false
 
 
-func _on_laser_cooldown_timeout():
+func _on_laser_timer_timeout():
 	can_laser = true
+
+func _on_hit_timer_timeout():
+	vulnerable = true

533c3d8c19d545f04a5207a6c6bc74634cf93c8d
V1rtualAlloc <kocovic.vladimir@gmail.com>
2024-04-29 22:48:00 +0200

    shaders

--- scenes/enemies/scout.gd
+++ scenes/enemies/scout.gd
@@ -14,6 +14,7 @@ func hit():
 		health -= 10
 		vulnerable = false
 		$Timers/HitTimer.start()
+		$Sprite2D.material.set_shader_parameter("progress", 1)
 	if health <= 0:
 		queue_free()
 
@@ -43,3 +44,4 @@ func _on_laser_timer_timeout():
 
 func _on_hit_timer_timeout():
 	vulnerable = true
+	$Sprite2D.material.set_shader_parameter("progress", 0)
